c09eb5a6b08e61fdcf4a9e50ac5f92828151327e
Neil Girdhar <mistersheik@gmail.com>
2022-01-17 16:36:57 -0500

    Improve run-all

--- run_all.py
+++ run_all.py
@@ -1,20 +1,29 @@
-from info import Info
-from tree import Tree
 from itertools import count
 
+from info import Info
+from tree import Tree
+
+hard_mode = False
 original_info = Info.create(history=[],
                             placed=' ' * 5,
                             extra_discovered='')
-tree = Tree.create(original_info, False, False)
+tree = Tree.create(original_info, hard_mode, False)
 
-for solution in tree.possible_solutions:
+total_i = 0
+worst = 0
+for n, solution in enumerate(tree.possible_solutions):
     info = original_info
     guess = 'raise'
-    print("Solution:", solution)
+    # print("Solution:", solution)
     for i in count(1):
-        print("  Guessing:", guess)
+        # print("  Guessing:", guess)
         info = info.add_guess(guess, solution)
         if guess == solution:
             break
-        other_tree = Tree.create(info, False, False)
+        other_tree = Tree.create(info, hard_mode, False)
         guess = other_tree.best_guess(info)
+    total_i += i
+    if 5 < i:
+        print(i, solution)
+    worst = max(worst, i)
+    print(n + 1, total_i / (n + 1), worst)
